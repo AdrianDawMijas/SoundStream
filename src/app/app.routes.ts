5af90e1bd33d2de1034a4e6fa08dcae2b7e33f51
adrian <agargon477@g.educaand.es>
2025-02-20 08:26:33 +0100

    BackGround Completo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component'; // Vista Landing
 import { LoginComponent } from './login/login.component';
+import {PricingComponent} from './pricing/pricing.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent }, // Ruta raíz (Landing)
   { path: 'login', component: LoginComponent },
+  { path: 'pricing', component: PricingComponent },
 ];
 
 @NgModule({
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit {
 
+  @ViewChild('bgVideo', { static: false }) videoElement!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    this.ensureVideoPlays();
+  }
+
+  ensureVideoPlays() {
+    const video = this.videoElement.nativeElement;
+
+    if (video) {
+      video.muted = true;  // Asegurar que está silenciado
+      video.loop = true;
+      video.autoplay = true;
+      video.playsInline = true;
+
+      video.play().catch((error) => {
+        console.error("No se pudo reproducir el video automáticamente:", error);
+      });
+
+      // Verificar si el video se pausa y reiniciarlo cada 3s
+      setInterval(() => {
+        if (video.paused) {
+          video.play();
+        }
+      }, 3000);
+    }
+  }
 }
